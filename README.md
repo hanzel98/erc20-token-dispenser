30cc5ff73df8e0bf83a6124804b38a76a80bd901
Hanzel Anchia Mena <hanzel.anchia@ideasoft.io>
2023-03-15 22:07:30 -0600

    feat: added function to claim, pending testing

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 Design and code a smart contract (or set of smart contracts) that will distribute ERC20 tokens over time, on a predetermined schedule.
 
-The system must provide a mechanism to distribute the tokens monthly. This could be in one or many payments, what is important is that under no circumstances we distribute more than the specified amount in any month.
+The system must provide a mechanism to distribute the tokens monthly. This could be in one or many payments.
+
+"It is important is that under no circumstances we distribute more than the specified amount in any month". Due to this restriction, if the receiver forgets to claim in some months, the system will only be able to pay what is available only in the current month.
 
 ### High-level system architecture
 
@@ -12,6 +14,7 @@ The system must provide a mechanism to distribute the tokens monthly. This could
 
 ### Math validation
 
+These mathematical validations represent what the claimed amounts would look like if the user claimed every month. But the logic of the contract considers that the user could forget to claim some months.
 ![MathValidation](MathValidation.png)
 
 ### Formula validation for years greater than 4
--- contracts/TokenDispenser.sol
+++ contracts/TokenDispenser.sol
@@ -9,56 +9,80 @@ contract TokenDispenser is Ownable {
     error InvalidToken();
     error InvalidReceiver();
     error InvalidMontlyMax();
+    error DistributionNotAllowedYet();
+    error MonthlyClaimTooHigh();
+    error NoTokensLeftToDistribute();
+    error PaymentFailed();
 
+    uint256 public constant ONE_YEAR = 365 days;
     IERC20 private immutable token;
     uint256 public immutable monthlyMin;
     uint256 public immutable monthlyMax;
     uint256 public immutable start;
     address public receiver;
+    uint256 public claimedThisMonth;
+    uint256 public lastClaimedPeriodStartTime;
 
+    event Claimed(uint256 amount);
     event ReceiverChanged(address oldReceiver, address newReceiver);
 
-    constructor(
-        IERC20 _token,
-        uint256 _monthlyMin,
-        uint256 _monthlyMax,
-        uint256 _start,
-        address _receiver
-    ) {
+    constructor(IERC20 _token, uint256 _monthlyMin, uint256 _monthlyMax, address _receiver) {
         if (address(_token) == address(0)) revert InvalidToken();
+        if (_monthlyMax == 0) revert InvalidMontlyMax();
         if (receiver == address(0)) revert InvalidReceiver();
-        if (receiver == address(0)) revert InvalidReceiver();
-        if (monthlyMax == 0) revert InvalidMontlyMax();
 
         token = _token;
         monthlyMin = _monthlyMin;
         monthlyMax = _monthlyMax;
-        start = _start;
         receiver = _receiver;
+        start = block.timestamp;
+        lastClaimedPeriodStartTime = block.timestamp;
     }
 
     function claim(uint256 _amount) external {
-        // TODO: Steps
+        // Steps
         // 1- Verify that the claimable amount is equal or greater than the input amount
         // 2- Discount any previously claimed amounts from the claimable amount
         // 3- Add the new claimed to the previous claimed amount of the month
         // 4- Transfer the final amount of tokens to the user
+        // TODO: Validate if it should count the time from the start not from the previous
+
+        uint256 elapsedTime = block.timestamp - start;
+        uint256 elapsedMonths = elapsedTime / (ONE_YEAR / 12);
+        if (elapsedMonths == 0) return;
+        uint256 newPeriodStartTime = elapsedMonths * (ONE_YEAR / 12);
+
+        (uint256 claimable, bool isLeftOver) = calculateMaxTokensThisMonth();
+        if (claimable == 0) revert NoTokensLeftToDistribute();
+
+        bool isNewMonth = newPeriodStartTime > lastClaimedPeriodStartTime;
+        if (isNewMonth) {
+            lastClaimedPeriodStartTime = newPeriodStartTime;
+            if (isLeftOver) _amount = claimable;
+            claimedThisMonth = _amount;
+        } else {
+            if (claimable < (_amount + claimedThisMonth)) revert MonthlyClaimTooHigh();
+        }
+
+        if (!token.transfer(msg.sender, _amount)) revert PaymentFailed();
+        emit Claimed(_amount);
     }
 
     function getContractBalance() external view returns (uint256) {
         return token.balanceOf(address(this));
     }
 
-    function getClaimableAmount() public view returns (uint256) {
-        uint256 amount = _getClaimableAmount();
+    function calculateMaxTokensThisMonth() public view returns (uint256 amount, bool isLeftOver) {
+        amount = _getClaimableAmount();
         assert(monthlyMax >= amount);
-        if (amount > monthlyMin) return amount;
-        return token.balanceOf(address(this));
+        if (amount <= monthlyMin) {
+            amount = token.balanceOf(address(this));
+            isLeftOver = true;
+        }
     }
 
     function _getClaimableAmount() private view returns (uint256) {
-        uint256 year = 365 days;
-        uint256 currentYear = (block.timestamp - start) / year;
+        uint256 currentYear = (block.timestamp - start) / ONE_YEAR;
         if (currentYear <= 1) return (monthlyMax * 10) / 100;
         if (currentYear < 5) {
             uint256 percentage = (currentYear == 2 ? 25 : (currentYear == 3 ? 50 : 100));
